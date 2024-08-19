bca5715d98449807d62607b5beac94de91fcd86e
StephenAnthony8 <anthonysteve02@gmail.com>
2024-08-20 01:50:23 +0300

    Question 1: webpack configurations

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,25 +1,6 @@
 import $ from "jquery";
 import _ from "lodash";
 
-
-
-
-
-/* $('body').append(`<p>Holberton Dashboard</p>`);
-$('body').append(`<p>Dashboard data for the students</p>`);
-$('body').append(`<button>Click here to get started</button>`);
-$('body').append(`<p id='count'></p>`);
-$('body').append(`<p>Copyright - Holberton School</p>`);
-
-let count = 0;
-function updateCounter() {
-  count++;
-  $("p#count").text(`${count} clicks on the button`);
-};
-
-$('button').click(_.debounce(updateCounter, 200));
- */
-
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
