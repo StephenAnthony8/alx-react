44e41a38f11be43f5bad800e736b89031b1af290
StephenAnthony8 <anthonysteve02@gmail.com>
2024-08-20 01:18:22 +0300

    Question 1: webpack configurations

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import $ from "jquery";
+var _ = require('lodash');
+
+let count = 0;
+function updateCounter() {
+  count++;
+  $("p#count").text(`${count} clicks on the button`);
+};
+
+
+$('document').ready(()=> {
+  $('body').append(`<p>Holberton Dashboard</p>`);
+  $('body').append(`<p>Dashboard data for the students</p>`);
+  $('body').append(`<button>Click here to get started</button>`);
+  $('body').append(`<p id='count'></p>`);
+  $('body').append(`<p>Copyright - Holberton School</p>`);
+
+
+  $('button').click(_.debounce(updateCounter, 200));
+});
+
+
