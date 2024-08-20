65099dfb45123b96ea7dc6e25017bef7b45e277f
StephenAnthony8 <anthonysteve02@gmail.com>
2024-08-20 15:57:09 +0300

    Question 2: asset modules

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -21,8 +21,16 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource'
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: true
+            }
+          }
+        ]
       }
     ]
   }
