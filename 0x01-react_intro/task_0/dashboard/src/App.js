1ea14937dbc6af0e0ed9bb97b5c7f4e03e42341e
StephenAnthony8 <anthonysteve02@gmail.com>
2024-08-29 06:25:27 +0300

    Question 0 & 1: React intro App Component

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import holbertonLogo from "./holberton-logo.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="App-header">
+        <img src={holbertonLogo} alt="holberton-logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+      
     </div>
   );
 }
