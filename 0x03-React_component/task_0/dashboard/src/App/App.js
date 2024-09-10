77b38c26132142d3ebf3cf8f36f3d12e45689394
StephenAnthony8 <anthonysteve02@gmail.com>
2024-09-10 11:37:32 +0300

    Question 0: React Class components

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -11,7 +11,8 @@ import { getLatestNotification } from '../utils/utils';
 
 /* Class component */
 class App extends React.Component {
-  constructor(props = { isLoggedIn: false }) {
+  /* initialization */
+  constructor(props = { isLoggedIn: false, logOut: () => {return null}}) {
     super(props);
     this.state = {
       listCourses: [
@@ -22,10 +23,14 @@ class App extends React.Component {
       listNotifications: [
         { id: 1, type: "default", value: "New course available" },
         { id: 2, type: "urgent", value: "New resume available" },
-        { id: 3, type: "urgent", html: { __html: getLatestNotification() } }
+        { id: 3, type: "urgent", html: { __html: getLatestNotification() }}
       ]
     };
+    
   }
+
+
+  /* rendering */
   render() {
     return (
       <>
@@ -44,38 +49,9 @@ class App extends React.Component {
   }
 }
 
-/* Function Component */
-/* function App({ isLoggedIn = false }) {
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 }
-  ];
-
-  const listNotifications = [
-    { id: 1, type: "default", value: "New course available" },
-    { id: 2, type: "urgent", value: "New resume available" },
-    { id: 3, type: "urgent", html: { __html: getLatestNotification() } }
-  ];
-
-  return (
-    <>
-      <Notifications listNotifications={listNotifications} />
-      <div className="App">
-
-        <>
-          <Header />
-          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-          <Footer />
-        </>
-
-      </div>
-    </>
-  );
-} */
-
 App.propTypes = {
-  isLoggedIn: PropTypes.bool
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func
 };
 
 export default App;
